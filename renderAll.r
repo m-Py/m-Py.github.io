8f356b7284fe20e32ec295416307ca2bc589f670
Martin Papenberg <martin.papenberg@hhu.de>
2017-12-18 14:15:15 +0100

    some restructuring of repository

--- renderAll.r
+++ renderAll.r
@@ -1,7 +1,9 @@
 ### I use this file to render all Rmd files to html
 
+setwd("./rmdsource")
 rmdFiles <- list.files(pattern=".Rmd")
+setwd("../")
 
 for (i in rmdFiles) {
-    rmarkdown::render(i)
+    rmarkdown::render(paste0("./rmdsource/", i), output_dir="../")
 }
