f930ad4f5b4d981a133c6eb4bec9841156623f95
Martin Papenberg <martin.papenberg@hhu.de>
2017-12-20 11:45:17 +0100

    some changes to the repo

--- rmdsource/rsource/functions.R
+++ rmdsource/rsource/functions.R
@@ -17,7 +17,7 @@ power_bf <- function(N, effect_size, nsim = 1000, rscale = sqrt(2)/2,
     info$quantile <- NULL
     info$power    <- probability
     if (say_result) {
-        cat("\n Approx.", probability*100,
+        cat("\n Approximately", probability*100,
             "% of all Bayes factors will be larger than",
             round(info$BF, 2), "\n\n")
     }
@@ -27,7 +27,7 @@ power_bf <- function(N, effect_size, nsim = 1000, rscale = sqrt(2)/2,
 ## Returns a data.frame in with columns N and BF; each BF is based on
 ## one draw in the simulation. `sample_sizes` is the »total sample
 ## size«, i.e. the sample size per group is `sample_sizes/2`. Note that
-## the log(BF) is returned!!!
+## the log(BF) is returned!
 estimate_expected_bf <- function(effect_size, sample_sizes,
                                  n_bayes_factors, rscale=sqrt(2)/2) {
     ## store BFs for each repetition for one sample size:
@@ -53,27 +53,30 @@ estimate_expected_bf <- function(effect_size, sample_sizes,
 
 ## processes data returned by `estimate_expected_bf` and returns Bayes
 ## factor quantiles by sample size as a data.frame. Note that the
-## log(BF) is returned!!!
-BF_quantiles <- function(dat, quantiles = c(0.025, 0.5, 0.975)) {
-    foo <- tapply(dat$logBF, dat$N, quantile, probs=quantiles, simplify = FALSE)
+## log(BF) is returned!
+BF_quantiles <- function(dat, quantiles = c(0.2, 0.5, 0.8)) {
+    foo <- tapply(dat$logBF, dat$N, quantile, probs=quantiles,
+                  simplify = FALSE)
     ## use plyr to convert array to data.frame
     foo <- adply(foo, 1)
     ## use reshape to create long data
     long_quantiles <- melt(foo, id.vars= c("X1"), factorsAsStrings=TRUE)
+    ## some ugly renaming:
     long_quantiles$N <- as.numeric(levels(long_quantiles$X1))[long_quantiles$X1]
     long_quantiles$X1 <- NULL
     long_quantiles$quantile <- long_quantiles$variable
     long_quantiles$variable <- NULL
     long_quantiles$logBF    <- long_quantiles$value
     long_quantiles$value    <- NULL
+    ## attach effect size and prior scale
     long_quantiles$eff_size <- unique(dat$eff_size)
     long_quantiles$rscale   <- unique(dat$rscale)
     return(long_quantiles)
 }
 
 plot_BF_quantiles <- function(BF_quantiles, thresholds=NULL, ylim=NULL,
-                              main = "", axis = c("log", "standard"),
-                              label_curves = TRUE) {
+                              main = "", axis = c("log", "standard")
+                              ) {
 
     ## first make some adjustments to the plot in dependence of which
     ## axis the user wants to show:
@@ -109,17 +112,13 @@ plot_BF_quantiles <- function(BF_quantiles, thresholds=NULL, ylim=NULL,
     ## add y-axis to the right if that was required by the user
     if ("standard" %in% axis)  add_normal_bf_scale()
 
-    # if (label_curves) label()
-
     ## indicate the neutral evidence line if it was required by the user
     cols = rep("darkgrey", length(thresholds))
     cols[which(thresholds==1)] <- "black"
     abline(h = log(thresholds), lty=2, lwd =1.5, col=cols)
-
-    
     
     ## reset margins
-    par(mar = def_mar)
+    on.exit(par(mar = def_mar))
 }
 
 add_normal_bf_scale <- function() {
@@ -129,9 +128,4 @@ add_normal_bf_scale <- function() {
     labs[labs >=1] <- round(labs[labs >=1], 1)
     axis(4, at = label_bf_lim, labels = labs, las=1)
     mtext("BF", 4, line = 3.1)
-}
-
-label_curves <- function() {
-    
-    
 }
