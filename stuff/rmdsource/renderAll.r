ed29aaf836ce57f9b94f27c5d66a8afae45c76b3
Martin Papenberg <martin.papenberg@hhu.de>
2019-11-06 09:32:50 +0100

    added post

--- stuff/rmdsource/renderAll.r
+++ stuff/rmdsource/renderAll.r
@@ -1,6 +1,6 @@
 ### I use this file to render all Rmd files to html
 
-rmdFiles <- list.files(pattern=".Rmd")
+rmdFiles <- setdiff(list.files(pattern=".Rmd"), "06_BFSize.Rmd")
 
 for (i in rmdFiles) {
     rmarkdown::render(i, output_dir="../")
