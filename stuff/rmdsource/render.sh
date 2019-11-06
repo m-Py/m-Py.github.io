b1e77231e96bd28d3fb53b94f089a23c275add4f
Martin Papenberg <martin.papenberg@hhu.de>
2019-11-06 13:22:01 +0100

    {

--- stuff/rmdsource/render.sh
+++ stuff/rmdsource/render.sh
@@ -1,9 +1,14 @@
-# This script renders all changed posts and commits the changes to git
-
-# has one parameter that is the commit message
-# If no commit message is passed, the changes are only staged and not committed
 
 #!/bin/sh
+
+# This script renders all changed posts and commits the changes to git
+
+# has one parameter that is the commit message, has to be passed in the form
+# If no commit message is passed, the changes are only staged and not committed
+
+# Usage
+# ./render.sh
+# ./render.sh { "this is my commit message" }
 
 R -e 'source("renderAll.r")'
 
