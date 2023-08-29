dba42d3d625963260aac238e8404e9e36a08c957
Martin Papenberg <martin.papenberg@hhu.de>
2023-08-29 13:33:35 +0200

    started anticlust blog

---
+++ anticlust-blog/render.sh
@@ -0,0 +1,36 @@
+
+#!/bin/sh
+
+# This script renders all changed posts and commits the changes to git
+
+# If no post was rendered, this script will still stage and commit --
+# there is no test if any file was actually changed. This may be 
+# convenient anyway
+
+# Has one parameter that is the commit message, has to be passed as shown in the example below
+# If no commit message is passed, the changes are only staged and not committed
+
+# Usage
+# ./render.sh
+# ./render.sh "this is my commit message"
+
+R -e 'source("renderAll.r")'
+
+# test if there have been changes to the repo, stage and maybe commit them:
+if ! git diff-index --quiet HEAD --; then  
+  cd ..
+  git add .
+  cd -
+  # test if changes should be committed
+  if [ $# -eq 0 ]
+    then
+      echo "Changes have been staged, but not committed"
+  else 
+    git commit -m "$1"
+    echo "Changes have been committed"
+  fi
+else 
+  echo "There have been no changes to the repository"
+fi
+
+git status
---
+++ anticlust-blog/renderAll.r
@@ -0,0 +1,26 @@
+# I use this file to render all Rmd files to html. It assumes that
+# files are changed/staged, but not committed
+
+# 
+staged_files  <- system("git diff --name-only --cached", intern = TRUE)
+changed_files <- system("git diff --name-only", intern = TRUE)
+new_files <- system("git ls-files --others --exclude-standard", intern = TRUE)
+
+files <- unique(c(staged_files, changed_files, new_files))
+
+
+# only select Rmd files
+rmds <- files[grepl(pattern =".Rmd", x = files)] 
+#rmds <- list.files(patter = ".Rmd")
+rmds <- sapply(strsplit(rmds, split = "/"), function(x) { x[grepl(x, pattern = ".Rmd")] })
+
+message("\nRendering ", c("no file", "file", "files")[length(rmds) + 1], " ", toString(rmds))
+
+for (i in rmds) {
+  rmarkdown::render(
+    i, 
+    output_dir=".",
+    output_format = "html_document",
+    output_options = list(theme = "united")
+  )
+}
